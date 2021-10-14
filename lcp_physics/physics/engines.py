adf785dd6ee774d060cf82ae19ca46ab392da720
Jiacheng <ethancooper007@gmail.com>
2021-10-14 13:41:00 -0500

    add bottom friction test scene

--- lcp_physics/physics/engines.py
+++ lcp_physics/physics/engines.py
@@ -34,20 +34,50 @@ class PdipmEngine(Engine):
         if neq > 0:
             u = torch.cat([u, u.new_zeros(neq)])
         if not world.contacts:
-            # No contact constraints, no complementarity conditions
+            
+            # Solve Mixed LCP (Kline 2.7.2)
+            Jc = world.Jc()
+            ncon = Jc.size(0)   # number of contacts
+            v = torch.matmul(Jc, world.get_v()) * world.restitutions()
+            M = world.M().unsqueeze(0)
             if neq > 0:
-                P = torch.cat([torch.cat([world.M(), -Je.t()], dim=1),
-                               torch.cat([Je, Je.new_zeros(neq, neq)],
-                                         dim=1)])
+                b = Je.new_zeros(Je.size(0)).unsqueeze(0)
+                Je = Je.unsqueeze(0)
             else:
-                P = world.M()
-            if self.cached_inverse is None:
-                inv = torch.inverse(P)
-                if world.static_inverse:
-                    self.cached_inverse = inv
-            else:
-                inv = self.cached_inverse
-            x = torch.matmul(inv, u)  # Kline Eq. 2.41
+                b = torch.tensor([])
+                Je = torch.tensor([])
+            Jc = Jc.unsqueeze(0)
+            u = u[:world.M().size(0)].unsqueeze(0)
+            v = v.unsqueeze(0)
+            E = world.E().unsqueeze(0)
+            mu_s = world.mu_s().unsqueeze(0)
+            Js = world.Js().unsqueeze(0)
+            Jb = world.Jb().unsqueeze(0)
+            # G = torch.cat([Jc, 
+            #                Js,
+            #                Js.new_zeros(Js.size(0), ncon, 3*nbody)
+            #             ], dim=1)
+            
+            G = torch.cat([Jc, 
+                           Js,
+                           Js.new_zeros(Js.size(0), mu_s.size(1), Js.size(2)),
+                           Jb,
+                           Jb.new_zeros(M.size(0), M.size(1), M.size(2))
+                        ], dim=1)
+            F = G.new_zeros(G.size(1), G.size(1)).unsqueeze(0)
+            F[:,   ncon:3*ncon, 3*ncon:4*ncon] = E
+            F[:, 3*ncon:4*ncon,       :  ncon] = mu_s
+            F[:, 3*ncon:4*ncon,   ncon:3*ncon] = -E.transpose(1, 2)
+            F[:,         4*ncon:4*ncon+2*nbody, 4*ncon+3*nbody:4*ncon+5*nbody] = \
+                                    0.99*torch.diag(G.new_ones(2*nbody)).unsqueeze(0)
+            F[:, 4*ncon+2*nbody:4*ncon+5*nbody,         4*ncon:4*ncon+3*nbody] = \
+                                   -0.99*torch.diag(G.new_ones(3*nbody)).unsqueeze(0)
+            h = torch.cat([v, 
+                           v.new_zeros(v.size(0), 3*ncon+2*nbody),
+                           (world.mu_b() * torch.diag(world.M()).unsqueeze(0))
+                        ], dim=1)   # m in Eq.(2)
+
+            x = -self.lcp_solver(max_iter=self.max_iter, verbose=-1)(M, u, G, h, Je, b, F)
         else:
             # Solve Mixed LCP (Kline 2.7.2)
             Jc = world.Jc()
@@ -83,9 +113,9 @@ class PdipmEngine(Engine):
             F[:, 3*ncon:4*ncon,       :  ncon] = mu_s
             F[:, 3*ncon:4*ncon,   ncon:3*ncon] = -E.transpose(1, 2)
             F[:,         4*ncon:4*ncon+2*nbody, 4*ncon+3*nbody:4*ncon+5*nbody] = \
-                                    torch.diag(G.new_ones(2*nbody)).unsqueeze(0)
+                                    1*torch.diag(G.new_ones(2*nbody)).unsqueeze(0)
             F[:, 4*ncon+2*nbody:4*ncon+5*nbody,         4*ncon:4*ncon+3*nbody] = \
-                                   -torch.diag(G.new_ones(3*nbody)).unsqueeze(0)
+                                   -1*torch.diag(G.new_ones(3*nbody)).unsqueeze(0)
             h = torch.cat([v, 
                            v.new_zeros(v.size(0), 3*ncon+2*nbody),
                            (world.mu_b() * torch.diag(world.M()).unsqueeze(0))
