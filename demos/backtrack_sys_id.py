3f992ed540a97a7fe5c1dbee9d09a460c4ec292e
Jiacheng <ethancooper007@gmail.com>
2022-01-01 13:01:44 -0600

    working on backtracking system id

---
+++ demos/backtrack_sys_id.py
@@ -0,0 +1,119 @@
+import os
+import sys
+
+import time
+import math
+from math import sin, cos
+import cv2
+import pygame
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import torch
+from torch.autograd import Variable
+
+from lcp_physics.physics.bodies import Circle, Rect, Hull, Composite
+from lcp_physics.physics.constraints import TotalConstraint, FixedJoint
+from lcp_physics.physics.forces import ExternalForce, Gravity, vert_impulse, hor_impulse
+from lcp_physics.physics.utils import Defaults, plot, reset_screen, Recorder
+from lcp_physics.physics.world import World, run_world
+from lcp_physics.physics.action import build_mesh, random_action
+from lcp_physics.physics.sim import SimSingle
+
+
+TIME = 2
+DT = Defaults.DT
+DEVICE = Defaults.DEVICE
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+np.random.seed(10)
+torch.random.manual_seed(0)
+
+def plot_mass_error(mask, m1, m2, save_path=None):
+    err = np.zeros_like(mask).astype('float')
+    err[mask] = m1 - m2
+
+    ax = plt.subplot()
+    im = ax.imshow(err, vmin=-0.2, vmax=0.2, cmap='plasma')
+
+    # create an axes on the right side of ax. The width of cax will be 5%
+    # of ax and the padding between cax and ax will be fixed at 0.05 inch.
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+
+    plt.colorbar(im, cax=cax)
+    if save_path:
+        plt.savefig(save_path)
+
+    plt.clf()
+    ax.cla()
+
+
+def sys_id_demo(screen):
+
+    if screen is not None:
+        import pygame
+        background = pygame.Surface(screen.get_size())
+        background = background.convert()
+        background.fill((255, 255, 255))
+
+    obj_name = 'drill'
+    mass_img_path = os.path.join(ROOT, 'fig/%s_mass.png'%obj_name)
+    bottom_fric_img_path = os.path.join(ROOT, 'fig/%s_fric.png'%obj_name)
+
+    sim = SimSingle.from_img(mass_img_path, bottom_fric_img_path, particle_radius=10, 
+                    hand_radius=20)
+    sim.bottom_fric_est = sim.bottom_fric_gt
+    sim.action_mag = 20
+    sim.force_time = 0.3
+    sim.mass_est = 0.09 * torch.ones(sim.N).to(DEVICE)
+    sim.mass_est = Variable(sim.mass_est, requires_grad=True)
+    
+    learning_rate = 1e-4
+    max_iter = 100
+
+    dist_hist = []
+    mass_err_hist = []
+    last_dist = 1e10
+    for i in range(max_iter):
+
+        rotation, offset, action, X1, X2 = sim.run_episode_random(t=TIME, screen=screen)
+
+        plot_mass_error(sim.mask, sim.mass_gt, 
+                        sim.mass_est.detach().numpy(), 'tmp/mass_err_%03d.png'%i)
+        
+        dist = torch.sum(torch.norm(X1 - X2, dim=1))
+        dist.backward()
+        grad = torch.nan_to_num(sim.mass_est.grad.data)
+        print(grad)
+        grad.clamp_(1/learning_rate * -2e-3, 1/learning_rate * 2e-3)
+
+        sim.mass_est = torch.clamp(sim.mass_est.data - learning_rate * grad, min=1e-5)
+        sim.mass_est.requires_grad=True
+
+        # print('\n bottom friction coefficient: ', mu.detach().cpu().numpy().tolist())
+        learning_rate *= 0.99
+        print(i, '/', max_iter, dist.data.item() / sim.N)
+        
+        print('=======\n\n')
+        dist_hist.append(dist / sim.N)
+
+        reset_screen(screen)
+
+    plot(dist_hist)
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == '-nd':
+        # Run without displaying
+        screen = None
+    else:
+        pygame.init()
+        width, height = 1000, 600
+        screen = pygame.display.set_mode((width, height), pygame.DOUBLEBUF)
+        screen.set_alpha(None)
+        pygame.display.set_caption('2D Engine')
+        reset_screen(screen)
+
+    sys_id_demo(screen)
