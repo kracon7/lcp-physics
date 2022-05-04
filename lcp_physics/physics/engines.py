c2e319ae1806b4d32d1b5dba601f82dd653b033b
jiacheng <ethancooper007@gmail.com>
2022-05-03 23:15:48 -0500

    working on tactile force feedback with friction considered

--- lcp_physics/physics/engines.py
+++ lcp_physics/physics/engines.py
@@ -27,6 +27,8 @@ class PdipmEngine():
         neq = Je.size(0) if Je.ndimension() > 0 else 0
         nbody = len(world.bodies)
 
+        nus = None
+
         f = world.apply_forces(t)
         u = torch.matmul(world.M(), world.get_v()) + dt * f
         if neq > 0:
@@ -74,7 +76,7 @@ class PdipmEngine():
             F[:, -mu_s.size(1):, mu_s.size(2):mu_s.size(2) + E.size(1)] = \
                 -E.transpose(1, 2)
             h = torch.cat([v, v.new_zeros(v.size(0), Js.size(1) + mu_s.size(1))], 1)   # m in Eq.(2)
-            x = -self.lcp_solver.apply(M, u, G, h, Je, b, F, self.lcp_options)
+            x, nus = -self.lcp_solver.apply(M, u, G, h, Je, b, F, self.lcp_options)
         elif extend:
             Jc = world.Jc()
             ncon = Jc.size(0)
@@ -113,9 +115,9 @@ class PdipmEngine():
                            (world.mu_b_diag_M().unsqueeze(0))
                           ], dim=1)   # m in Eq.(2)
 
-            x = -self.lcp_solver.apply(M, u, G, h, Je, b, F, self.lcp_options)
+            x, nus = -self.lcp_solver.apply(M, u, G, h, Je, b, F, self.lcp_options)
         new_v = x[:world.vec_len * len(world.bodies)].squeeze(0)
-        return new_v, x
+        return new_v, x, nus
 
     def post_stabilization(self, world):
         v = world.get_v()
@@ -151,6 +153,6 @@ class PdipmEngine():
             M = M.unsqueeze(0)
             v = v.unsqueeze(0)
             F = Jc.new_zeros(Jc.size(1), Jc.size(1)).unsqueeze(0)
-            x = self.lcp_solver.apply(M, h, Jc, v, Je, b, F, self.lcp_options)
+            x, _ = self.lcp_solver.apply(M, h, Jc, v, Je, b, F, self.lcp_options)
         dp = -x[:M.size(0)]
         return dp
