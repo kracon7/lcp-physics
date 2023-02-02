b54e3ef91cc10781187d075a98c47c35f6a19762
Jiacheng <ethancooper007@gmail.com>
2023-02-02 14:48:30 -0600

    Remove constraint force output in from forward sim

--- lcp_physics/lcp/lcp.py
+++ lcp_physics/lcp/lcp.py
@@ -37,40 +37,40 @@ class LCPFunction(Function):
         ctx.neq, ctx.nineq, ctx.nz = neq, nineq, nz
 
         ctx.Q_LU, ctx.S_LU, ctx.R = pdipm.pre_factor_kkt(Q, G, F, A)
-        zhats, nus, ctx.lams, ctx.slacks = pdipm.forward(
+        zhats, ctx.nus, ctx.lams, ctx.slacks = pdipm.forward(
             Q, p, G, h, A, b, F, ctx.Q_LU, ctx.S_LU, ctx.R,
             eps=lcp_options.eps, max_iter=lcp_options.max_iter, verbose=lcp_options.verbose,
             not_improved_lim=lcp_options.not_improved_lim, solver=lcp_options.solver)
 
         ctx.save_for_backward(zhats, Q, p, G, h, A, b, F)
-        return zhats, nus
+        return zhats
 
-    # @staticmethod
-    # def backward(ctx, dl_dzhat):
-    #     zhats, Q, p, G, h, A, b, F = ctx.saved_tensors
-    #     batch_size = extract_batch_size(Q, p, G, h, A, b)
+    @staticmethod
+    def backward(ctx, dl_dzhat):
+        zhats, Q, p, G, h, A, b, F = ctx.saved_tensors
+        batch_size = extract_batch_size(Q, p, G, h, A, b)
 
-    #     neq, nineq, nz = ctx.neq, ctx.nineq, ctx.nz
+        neq, nineq, nz = ctx.neq, ctx.nineq, ctx.nz
 
-    #     # D = torch.diag((ctx.lams / ctx.slacks).squeeze(0)).unsqueeze(0)
-    #     d = ctx.lams / ctx.slacks
+        # D = torch.diag((ctx.lams / ctx.slacks).squeeze(0)).unsqueeze(0)
+        d = ctx.lams / ctx.slacks
 
-    #     pdipm.factor_kkt(ctx.S_LU, ctx.R, d)
-    #     dx, _, dlam, dnu = pdipm.solve_kkt(ctx.Q_LU, d, G, A, ctx.S_LU,
-    #                                        dl_dzhat, G.new_zeros(batch_size, nineq),
-    #                                        G.new_zeros(batch_size, nineq),
-    #                                        G.new_zeros(batch_size, neq))
+        pdipm.factor_kkt(ctx.S_LU, ctx.R, d)
+        dx, _, dlam, dnu = pdipm.solve_kkt(ctx.Q_LU, d, G, A, ctx.S_LU,
+                                           dl_dzhat, G.new_zeros(batch_size, nineq),
+                                           G.new_zeros(batch_size, nineq),
+                                           G.new_zeros(batch_size, neq))
 
-    #     dps = dx
-    #     dGs = (bger(dlam, zhats) + bger(ctx.lams, dx))
-    #     dFs = -bger(dlam, ctx.lams)
-    #     dhs = -dlam
-    #     if neq > 0:
-    #         dAs = bger(dnu, zhats) + bger(ctx.nus, dx)
-    #         dbs = -dnu
-    #     else:
-    #         dAs, dbs = None, None
-    #     dQs = 0.5 * (bger(dx, zhats) + bger(zhats, dx))
+        dps = dx
+        dGs = (bger(dlam, zhats) + bger(ctx.lams, dx))
+        dFs = -bger(dlam, ctx.lams)
+        dhs = -dlam
+        if neq > 0:
+            dAs = bger(dnu, zhats) + bger(ctx.nus, dx)
+            dbs = -dnu
+        else:
+            dAs, dbs = None, None
+        dQs = 0.5 * (bger(dx, zhats) + bger(zhats, dx))
 
-    #     return dQs, dps, dGs, dhs, dAs, dbs, dFs, None
+        return dQs, dps, dGs, dhs, dAs, dbs, dFs, None
 
